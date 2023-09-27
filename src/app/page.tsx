4af49ce522dd7951c1424c1042e4267087cf673a
Tiago <tiagoff2012@gmail.com>
2023-09-27 14:14:11 -0300

    Create first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import { Card } from "./components/ui/Card";
+import Header from "@/components/Header";
+import { Card } from "@/components/ui/Card";
 
 export default function Home() {
     return (
